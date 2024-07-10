d28c29dda1b3986b367c89a4022393e3037e5c9d
teerenzo <renzahoemmanuel8@gmail.com>
2024-07-10 12:43:41 +0200

    ecrypt password

--- src/main/java/com/renzo/springboot/todoApp1/security/SpringSecurityConfiguration.java
+++ src/main/java/com/renzo/springboot/todoApp1/security/SpringSecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.renzo.springboot.todoApp1.security;
 
 import java.util.Collection;
+import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -14,11 +17,17 @@ public class SpringSecurityConfiguration {
 	
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
-		UserDetails userDetail= User.withDefaultPasswordEncoder()
+		Function<String, String> passwordEncoder= input -> passowrdEncoder().encode(input);
+		UserDetails userDetail= User.builder().passwordEncoder(passwordEncoder)
 				                .username("renzo")
 				                .password("dummy")
 				                .build();
 		return new InMemoryUserDetailsManager(userDetail);
+	}
+	
+	@Bean 
+	public PasswordEncoder passowrdEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
